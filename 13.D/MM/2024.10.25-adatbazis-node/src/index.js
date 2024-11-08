48a00762fad5e548d5fff2107a2ae8ec7a10db9a
mcitomi <berdotamas5@gmail.com>
2024-11-08 12:43:27 +0100

    backend mysql2 teacher courses

--- 13.D/MM/2024.10.25-adatbazis-node/src/index.js
+++ 13.D/MM/2024.10.25-adatbazis-node/src/index.js
@@ -38,15 +38,15 @@ app.get("/teachers", async (req, res) => {  // megadjuk az arrow func.-nek hogy 
         res.json(results);
 
     } catch (error) {
-        if(error.message.includes("Parameter error")) {    // ellenőrizzük hogy a mi hibánkat dobtuk-e
+        if (error.message.includes("Parameter error")) {    // ellenőrizzük hogy a mi hibánkat dobtuk-e
             res.status(400).json({
-                "message" : error.message
+                "message": error.message
             });
             return;
-        } 
+        }
 
         res.status(500).json({  // minden mas hiba kezelése "Fallback" válasz
-            "message" : "Internal server error, couldn't query teachers"
+            "message": "Internal server error, couldn't query teachers"
         });
     }
 });
@@ -55,29 +55,135 @@ app.get("/teachers/:id", async (req, res) => {
     try {
         const id = parseInt(req.params.id);   // útvonal paraméter lekerese
 
-        if(isNaN(id)) { // ha a parseInt nem számot kapott, Nan (Not a Number) az értéke
+        if (isNaN(id)) { // ha a parseInt nem számot kapott, Nan (Not a Number) az értéke
             throw new Error("Parameter error: 'id' must be a valid integer.");
         }
 
         if (id < 0) {
-            throw new Error("Parameter error: 'id' must be greater than 0.");   
+            throw new Error("Parameter error: 'id' must be greater than 0.");
         }
 
-        const [results, ] = await pool.query("SELECT * FROM teachers WHERE id = ?", [id]);
+        const [results,] = await pool.query("SELECT * FROM teachers WHERE id = ?", [id]);
 
         res.json(results);
 
     } catch (error) {
-        if(error.message.includes("Parameter error")){
+        if (error.message.includes("Parameter error")) {
             res.status(400).json({
-                "message" : error.message
+                "message": error.message
             });
             return;
         }
 
         // Fallback
         res.status(500).json({
-            "message" : "Something went wrong!"
+            "message": "Something went wrong!"
+        });
+    }
+});
+
+app.get("/courses", async (req, res) => {
+    try {
+        const teacherId = parseInt(req.query.teacher);
+
+        if (teacherId && isNaN(teacherId)) {
+            throw new Error("Parameter error: Invalid teacher parameter, must be a number!");
+        }
+
+        if (teacherId < 0) {
+            throw new Error("Parameter error: Teacher id must be a positive number!");
+        }
+
+        let results;
+
+        if (teacherId) {
+            [results,] = await pool.query(`
+                SELECT courses.id, courses.name, description, length, teachers.name as 'teacher' 
+                FROM courses 
+                INNER JOIN teachers ON teachers.id = courses.teacher_id
+                WHERE teachers.id = ?;
+            `, [teacherId]);
+        } else {
+            [results,] = await pool.query(`
+                SELECT courses.id, courses.name, description, length, teachers.name as 'teacher' 
+                FROM courses 
+                INNER JOIN teachers ON teachers.id = courses.teacher_id;
+            `);
+        }
+
+        res.json(results);
+
+    } catch (error) {
+        if (error.message.includes("Parameter error")) {
+            res.status(400).json({
+                "message": error.message
+            });
+            return;
+        }
+
+        res.status(500).json({
+            "message": "Cannot query courses!"
+        });
+    }
+});
+
+app.post("/courses", async (req, res) => {
+    try {
+        const body = req.body;
+
+        if (!body || typeof (body) !== "object" || Object.keys(body).length < 1) {
+            throw new Error("Server says: Invalid body!");
+        }
+
+        if (!body.name || typeof (body.name) !== "string") {
+            throw new Error("Server says: Invalid name field");
+        }
+
+        if (!body.description || typeof (body.description) !== "string") {
+            throw new Error("Server says: Invalid name field");
+        }
+
+        if (!body.length || typeof (body.length) !== "number") {
+            throw new Error("Server says: Invalid length field");
+        }
+
+        if (!body.teacher_id || typeof (body.teacher_id) !== "number") {
+            throw new Error("Server says: Invalid teacher_id field");
+        }
+
+        const [result, ] = await pool.execute(`
+            SELECT name FROM teachers WHERE id = ?;
+            `,
+            [body.teacher_id]
+        );
+        
+        if(Object.keys(result).length == 0) {
+            throw new Error("Server says: This teacher id do not exists!");
+        }
+
+        await pool.execute(`
+            INSERT INTO courses (id, name, description, length, teacher_id) 
+            VALUES (?, ?, ?, ?, ?);`, 
+            ['', body.name, body.description, body.length, body.teacher_id]
+        );
+
+        res.status(201).json({
+            "message" : "Course added!"
+        });
+        
+    } catch (error) {
+        if (error.message.includes("Server says")) {
+            res.status(400).json({
+                "message": error.message
+            });
+            return;
+        }
+
+        // Fallback
+        console.log(error);
+
+        res.status(500).json({
+            "message": "Something went wrong! Couldn't insert into table!"
         });
     }
 });
@@ -86,40 +192,40 @@ app.post("/teachers", async (req, res) => {
     try {
         const body = req.body;
 
-        if(!body || typeof(body) !== "object" || Object.keys(body).length !== 2) {  
+        if (!body || typeof (body) !== "object" || Object.keys(body).length !== 2) {
             // Ellenőrizzük hogy a body létezik-e, objetum-e és a body annyi értéket tartalmaz amennyit várunk, se többet se kevesebbet.
             throw new Error("Server says: Invalid body!");
         }
 
-        if(!body.name || typeof(body.name) !== "string") {
+        if (!body.name || typeof (body.name) !== "string") {
             throw new Error("Server says: Invalid name field");
         }
 
-        if(!body.age || typeof(body.age) !== "number") {
+        if (!body.age || typeof (body.age) !== "number") {
             throw new Error("Server says: Invalid age field");
         }
 
-        if(body.age < 18) {
+        if (body.age < 18) {
             throw new Error("Server says: Under 18! No way :o");
         }
 
-        const [result, ] = await pool.query("INSERT INTO teachers (name, age) VALUES (?, ?) returning *;", [body.name, body.age]);
+        const [result,] = await pool.query("INSERT INTO teachers (name, age) VALUES (?, ?) returning *;", [body.name, body.age]);
 
         res.status(201).json(result);
-        
+
     } catch (error) {
-        if(error.message.includes("Server says")){
+        if (error.message.includes("Server says")) {
             res.status(400).json({
-                "message" : error.message
+                "message": error.message
             });
             return;
         }
 
         // Fallback
         console.log(error);
-        
+
         res.status(500).json({
-            "message" : "Something went wrong! Couldn't insert into table!"
+            "message": "Something went wrong! Couldn't insert into table!"
         });
     }
 });
@@ -128,8 +234,8 @@ app.listen(PORT, () => {
     console.info(`Server started on port ${PORT}!`);
 });
 
-// HF: tanarok-ra, PUT, PATCH, DELETE + Frontend cucc 
+// HF: tanarok-ra, PUT, PATCH, DELETE + Frontend cucc
 // pl egy táblázat amibe lehet szerkeszteni őket
 
-// pl discordon: message update egy PATCH 
+// pl discordon: message update egy PATCH
 // a slashCommand update egy PUT
