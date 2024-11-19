25945a78a4ad08900c18eefa38216455f36e3f80
mcitomi <berdotamas5@gmail.com>
2024-11-19 12:15:59 +0100

    simple react app

--- 13.D/MM/2024.11.19-react-komponensek/src/App.tsx
+++ 13.D/MM/2024.11.19-react-komponensek/src/App.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyButton({alertText, handleHover}) {
+    function handleClick() {
+        alert(alertText);
+    }
+
+    return <button onClick={handleClick} onMouseEnter={handleHover}>{alertText}</button>;
+}
 
 export default function Main() {
+    // let [message, editMessage] = useState("Meow!");
+    // setTimeout(() => {
+    //     if(message == "Meow!") {
+    //         editMessage("Szia!");
+    //     } else {
+    //         editMessage("Meow!");
+    //     }
+    // }, 1000);
+
+    function buttonHover() {
+        console.log("HOVER");
+        
+    }
+
     return (
         <>
-            <div>asd</div>
-            <div>sad</div>
+            <h1>Hello!</h1>
+            <MyButton alertText="Kattintva a gomb" handleHover={buttonHover} ></MyButton>
+            <MyButton alertText="Kattintva a gomb 2!" handleHover={buttonHover}></MyButton>
         </>
     )
 }
