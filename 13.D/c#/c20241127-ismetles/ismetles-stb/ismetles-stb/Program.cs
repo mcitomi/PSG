8cc14e960e51b18d1141daefff58ce00fe60ea04
Tomi <admin@mcitomi.hu>
2024-12-06 10:46:04 +0100

    tanaros final

---
+++ 13.D/c#/c20241127-ismetles/ismetles-stb/ismetles-stb/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ismetles_stb
+{
+    public class Ember
+    {
+        private int _eletkora;
+        public int Eletkora { 
+            get
+            {
+                return _eletkora;
+            } 
+            private set
+            {
+                // value -> beérkező adat (= jel után)
+
+                if (!(value > _eletkora + 1))
+                {
+                    _eletkora = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Nem megengedett életkor!");
+                }
+            }
+        }
+
+        private string _nev;
+        public string Nev { 
+            get
+            {
+                if(Eletkora < 18)
+                {
+                    return $"Kiskorú: {_nev}";
+                } else
+                {
+                    return $"Nagykorú: {_nev}\t{Foglalkozas}";
+                }
+            }
+            set
+            {
+                if (value.Contains(' '))
+                {
+                    _nev = value;
+                } else
+                {
+                    throw new ArgumentException("A névnek tartalmaznia kell egy space karaktert!");
+                }
+            }
+        }
+        public string Foglalkozas { get; set; }
+
+        public Ember(string nev)    // konstruktor
+        {
+            Eletkora = 0;
+            Nev = nev;
+            Foglalkozas = "Nincs";
+
+        }
+
+        public void EletkorBeallit(int eletkor)
+        {
+            Eletkora = eletkor;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ember ember = new Ember("Schrei Boti");
+            Console.WriteLine(ember.Eletkora);
+
+            ember.EletkorBeallit(1);
+            ember.Nev = "Schrei Boti";
+
+            Console.WriteLine(ember.Eletkora);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 13.D/c#/c20241127-ismetles/ismetles-stb/ismetles-stb/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ismetles-stb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ismetles-stb")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("19a2225f-64fc-4ae9-b51d-07b9bc1b4e79")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
