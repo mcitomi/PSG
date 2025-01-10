e36e181a5c64aec8d14c69519026078efcff8487
Tomi <admin@mcitomi.hu>
2025-01-10 09:26:14 +0100

    2025 c# leitlivel + sql

---
+++ 13.D/c#/2025.01.07-fifa-leitli/fifa0107/fifa0107/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace fifa0107
+{
+    public struct fifa
+    {
+        public string csapat;
+        public int helyezes;
+        public int valtozas;
+        public float pontszam;
+    }
+    
+    public struct statisztika
+    {
+        public int helyValtozas;
+        public int csapatokSzama;
+    }
+
+    public partial class Form1 : Form
+    {
+        public List<fifa> fifaList = new List<fifa>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader reader = new StreamReader("../fifa.txt");
+
+            reader.ReadLine();  // fejléc soron átlépünk
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+
+                string[] values = line.Split(';');
+
+                fifa fifaValues = new fifa();
+                fifaValues.csapat = values[0];
+                fifaValues.helyezes = Convert.ToInt32(values[1]);
+                fifaValues.valtozas = Convert.ToInt32(values[2]);
+                fifaValues.pontszam = Convert.ToInt32(values[3]);
+
+                fifaList.Add(fifaValues);
+
+                listBox1.Items.Add(values[0]);
+                listBox2.Items.Add(values[1]);
+                listBox3.Items.Add(values[2]);
+                listBox4.Items.Add(values[3]);
+            }
+
+            MessageBox.Show($"A forrásállományban {fifaList.Count} csapat szerepel!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(fifaList.Count == 0)
+            {
+                MessageBox.Show("Hiba: Az adatok nincsenek beolvasva!");
+                return;
+            }
+
+            float pontszamok = 0;
+            foreach (fifa csapat in fifaList)
+            {
+                pontszamok += csapat.pontszam;
+            }
+
+            float atlaguk = pontszamok / fifaList.Count;
+
+            MessageBox.Show($"A csapatok átlagos pontszáma: {atlaguk.ToString()} pont");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int valtozas = -10;
+            fifa csapat = new fifa();
+            foreach (fifa legtobbetJavitoCsapat in fifaList)
+            {
+                if(legtobbetJavitoCsapat.valtozas > valtozas)
+                {
+                    csapat = legtobbetJavitoCsapat;
+                    valtozas = legtobbetJavitoCsapat.valtozas;
+                    Console.WriteLine(csapat.csapat);
+                }
+            }
+
+            MessageBox.Show($"A legtöbbet javító csapat {csapat.csapat} \n - Helyezés: {csapat.helyezes}\n - Pontszám: {csapat.pontszam}");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bool talalhatoMagyarCsapat = false;
+            foreach (fifa csapat in fifaList)
+            {
+                if(csapat.csapat.ToLower() == "magyarország")
+                {
+                    talalhatoMagyarCsapat = true;
+                }
+            }
+
+            if(talalhatoMagyarCsapat)
+            {
+                MessageBox.Show("Található magyar csapat a listán");
+            } else
+            {
+                MessageBox.Show("Nem található magyar csapat a listán");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            List<statisztika> stats = new List<statisztika>();
+
+            foreach (fifa csapat in fifaList)
+            {
+                statisztika statsValue = stats.Find(x => x.helyValtozas == csapat.valtozas);
+
+                Console.WriteLine(csapat.valtozas);
+
+                if(statsValue.csapatokSzama == 0)
+                {
+                    Console.WriteLine("fut");
+                    statisztika adatok = new statisztika();
+                    adatok.helyValtozas = csapat.valtozas;
+                    adatok.csapatokSzama = 1;
+                    stats.Add(adatok);
+                } else
+                {
+                    stats.Remove(statsValue);
+                    statsValue.csapatokSzama++;
+                    stats.Add(statsValue);
+                }
+            }
+
+            string statisztikaGui = "";
+            foreach (var item in stats)
+            {
+                Console.WriteLine($"{item.csapatokSzama} {item.helyValtozas}");
+
+                if(item.csapatokSzama > 1)
+                {
+                    statisztikaGui += $"{item.helyValtozas} helyet változott: {item.csapatokSzama} csapat\n";
+                }
+            }
+
+            MessageBox.Show(statisztikaGui);
+        }
+    }
+}
---
+++ 13.D/c#/2025.01.10-snooker-leitli/Snooker_0110/Snooker_0110/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Snooker_0110
+{
+    public partial class Form1 : Form
+    {
+        public struct snooker
+        {
+            public int helyezes;
+            public string nev;
+            public string orszag;
+            public int nyeremeny;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<snooker> list = new List<snooker>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader reader = new StreamReader("../../snooker.txt");
+
+            reader.ReadLine(); // fejléc sor skip
+
+            while (reader.Peek() >= 0) 
+            {
+                string line = reader.ReadLine();
+
+                listBox1.Items.Add(line);
+
+                string[] lineValues = line.Split(';');
+
+                snooker versenyzo = new snooker();
+                versenyzo.helyezes = int.Parse(lineValues[0]);
+                versenyzo.nev = lineValues[1];
+                versenyzo.orszag = lineValues[2];
+                versenyzo.nyeremeny = int.Parse(lineValues[3]);
+                
+                list.Add(versenyzo);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"A világranglistán {list.Count} versenyző szerepel");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double sum = 0;
+            foreach (snooker versenyzo in list)
+            {
+                sum += versenyzo.nyeremeny;
+            }
+            
+            MessageBox.Show($"A versenyzők átlagosan {Math.Round(sum / list.Count, 2):0.00} forintot kerestek");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int legnagyobbKereset = 0;
+            snooker keresettversenyzo = new snooker();
+
+            foreach (snooker versenyzo in list)
+            {
+                if(versenyzo.nyeremeny > legnagyobbKereset && versenyzo.orszag == "Kína")
+                {
+                    keresettversenyzo = versenyzo;
+                    legnagyobbKereset = versenyzo.nyeremeny;
+                }   
+            }
+
+            MessageBox.Show($"A legjobban kereső kínai versenyző:\n\tHelyezés: {keresettversenyzo.helyezes}\n\tNév: {keresettversenyzo.nev}\n\tOrszág: {keresettversenyzo.orszag}\n\tNyeremény összege: {(keresettversenyzo.nyeremeny * 380):0 000 000} Ft");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            bool isFound = false;
+            foreach (snooker versenyzo in list)
+            {
+                if(versenyzo.orszag == "Norvégia")
+                {
+                    isFound = true;
+                }
+            }
+
+            if (isFound)
+            {
+                MessageBox.Show("A versenyzők között van norvég versenyző");
+            } else
+            {
+                MessageBox.Show("A versenyzők között nincs norvég versenyző");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, int> stats = new Dictionary<string, int>();
+
+            foreach (snooker versenyzo in list)
+            {
+                if (stats.ContainsKey(versenyzo.orszag))
+                {
+                    stats[versenyzo.orszag]++;
+                } else
+                {
+                    stats.Add(versenyzo.orszag, 1);
+                }
+            }
+
+            StringBuilder statsString = new StringBuilder();
+            statsString.Append("Statisztika:\n");
+
+            foreach (var orszagstat in stats)
+            {
+                if(orszagstat.Value > 4)
+                {
+                    statsString.AppendLine($"\t{orszagstat.Key} - {orszagstat.Value} fő");
+                }
+            }
+
+            MessageBox.Show(statsString.ToString());
+        }
+    }
+}
