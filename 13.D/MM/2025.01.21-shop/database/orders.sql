b986bb9d852a6c1c780f8d1f9c9a0e82c912c83f
Tomi <admin@mcitomi.hu>
2025-01-22 11:54:49 +0100

    c# pilotak - dani

---
+++ 13.D/MM/2025.01.21-shop/database/orders.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2025. Jan 21. 09:45
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `orders`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `products`
+--
+
+INSERT INTO `products` (`id`, `name`) VALUES
+(1, 'Milánói sertésborda'),
+(2, 'Coca Cola'),
+(3, 'Kukoricás Penne Gyros feta sajttal'),
+(4, 'XIXO eper');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'Máté', 'milanoi_sertesborda'),
+(2, 'Laci', 'hat_izsak');
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`,`user_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `product_id_2` (`product_id`,`user_id`);
+
+--
+-- A tábla indexei `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ 13.D/c#/20250122-pilotak-dani/20250122-pilotak-dani/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _20250122_pilotak_dani
+{
+    struct pilota {
+        public string nev;
+        public string szuletesi_datum;
+        public string nemzetiseg;
+        public string rajtszam;
+    }
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<pilota> pilotaList = new List<pilota>();
+
+            StreamReader sr = new StreamReader("../../pilotak.csv");
+            sr.ReadLine();
+
+            while (sr.Peek() >= 1)
+            {
+                string[] line = sr.ReadLine().Split(';');
+
+                pilotaList.Add(new pilota { 
+                    nev = line[0], 
+                    szuletesi_datum = line[1], 
+                    nemzetiseg = line[2], 
+                    rajtszam = line[3] 
+                });
+            }
+
+            //foreach (pilota pilotaValues  in pilotaList)
+            //{
+            //    Console.WriteLine(pilotaValues.nev);
+            //}
+
+            Console.WriteLine($"3. feladat: {pilotaList.Count}");
+
+            Console.WriteLine($"4. feladat: {pilotaList[pilotaList.Count-1].nev}");
+
+            Console.WriteLine("5. feladat:");
+
+            pilota legkisebbRajtszamu = new pilota {
+                nev = "",
+                szuletesi_datum = "",
+                rajtszam = "1000",
+                nemzetiseg = ""
+            };
+
+            Dictionary<int, int> rajtszamok = new Dictionary<int, int>();
+
+            foreach (pilota pilotaValues in pilotaList)
+            {
+
+                int pilotaBirthYear = Convert.ToInt16(pilotaValues.szuletesi_datum.Split('.')[0]);
+                if (pilotaBirthYear < 1901)
+                {
+                    Console.WriteLine($"\t{pilotaValues.nev} ({pilotaValues.szuletesi_datum}.)");
+                }
+
+                if(int.TryParse(pilotaValues.rajtszam, out int pilotaRajtszam))
+                {
+                    if (int.Parse(legkisebbRajtszamu.rajtszam) > pilotaRajtszam)
+                    {
+                        legkisebbRajtszamu = pilotaValues;
+                    }
+
+                    if(rajtszamok.ContainsKey(pilotaRajtszam))
+                    {
+                        rajtszamok[pilotaRajtszam]++;
+                    } else
+                    {
+                        rajtszamok.Add(pilotaRajtszam, 1);
+                    }
+                }
+            }
+
+            Console.WriteLine($"6. feladat: {legkisebbRajtszamu.nemzetiseg}");
+
+            Console.Write("7. feladat: ");
+            foreach (var rajtszam in rajtszamok)
+            {
+                if(rajtszam.Value > 1)
+                {
+                    Console.Write($"{rajtszam.Key} ");   
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
