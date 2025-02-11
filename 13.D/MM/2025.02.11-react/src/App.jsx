ad05d4a8b8d536179daef98d40a9683911826ad1
Tomi <admin@mcitomi.hu>
2025-02-11 11:45:24 +0100

    mate cart

---
+++ 13.D/MM/2025.02.11-react/src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+function ProductCard({name, desc, price, onCLick}) {
+    return <>
+        <h2>{name}</h2>
+        <p>{desc}</p>
+        <b>{price}</b>
+        <button onClick={onCLick}>Kosárba</button>
+    </>
+}
+
+export default () => {
+    const products = [
+        {
+            "name": "Póló",
+            "desc": "Rövidujjú Nirvanás póló",
+            "price": 8000
+        },
+        {
+            "name": "Pulcsi",
+            "desc": "Hosszúujjú Bulbasauros kapucnis pulcsi",
+            "price": 15000
+        },
+        {
+            "name": "Nadrág",
+            "desc": "Kék farmernadrág",
+            "price": 10000
+        }
+    ];
+
+    const [cart, setCart] = useState([]);
+
+    function addToCart(i) {
+        let newCart = [...cart];
+        newCart.push(products[i]);
+        setCart(newCart);
+    }
+
+    return <>
+        <h1>Termékek</h1>
+        {
+            products.map(product => {
+                return <ProductCard name={product.name} desc={product.desc} price={product.price} onCLick={() => addToCart()}/>
+            })
+        }
+        <h1>Kosár</h1>
+        {
+            cart.map(product => {
+                return <ProductCard name={product.name} desc={product.desc} price={product.price} />
+            })
+        }
+    </>
+}
