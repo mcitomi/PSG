872a75dcb12f75c4c9d9901939a69fe52a741625
Tomi <admin@mcitomi.hu>
2025-03-13 10:30:19 +0100

    c# Morze és focistak

---
+++ 13.D/c#/20250312-MORZE/20250312-MORZE/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _20250312_MORZE
+{
+    class MorzeKod
+    {
+        public char Kulcs { get; set; }
+        public string Kod { get; set; }
+
+        public MorzeKod(char kulcs, string kod) { 
+            Kulcs= kulcs;
+            Kod= kod;
+        }
+    }
+
+    class Idezet
+    {
+        public string Idezete {  get; set; }
+        public string Kolto {  get; set; }
+
+        public Idezet (string idezet, string kolto)
+        {
+            Idezete = idezet;
+            Kolto = kolto;
+        }
+    }
+
+    internal class Program
+    {
+        internal static List<MorzeKod> morzeKods = new List<MorzeKod>();
+        internal static List<Idezet> morzeIdezetek = new List<Idezet>();
+        static string Morze2Szöveg(string morzeSzoveg)
+        {
+            int kihagyottKarakterek = 0;
+           
+            string dekodoltSzoveg = "";
+
+            //string[] morzeSor = morzeSzoveg.Split(' ');
+            
+            for (int i = 0; i < morzeSzoveg.Length; i++)
+            {
+                if (morzeSzoveg[i] == ' ')
+                {
+                    kihagyottKarakterek++;
+
+                    if(kihagyottKarakterek == 7)
+                    {
+                        dekodoltSzoveg += " ";
+                    }
+
+                } else
+                {
+                    kihagyottKarakterek = 0;
+                    //dekodoltSzoveg += morzeKods.Find(kod => kod.Kod == morzeSzoveg[i]).Kulcs;
+                }
+            }
+            return dekodoltSzoveg;
+        }
+
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("../../morzeabc.txt");
+
+            reader.ReadLine();
+
+            while (!reader.EndOfStream)
+            {
+                string[] elements = reader.ReadLine().Split('\t');
+
+                morzeKods.Add(new MorzeKod(elements[0][0], elements[1]));
+            }   // elements[0][0] -> első string elem első betűje
+
+            reader.Close();
+
+            // 3. feladat
+            Console.WriteLine($"Összesen {morzeKods.Count} karakter van a fájlban.");
+
+            // 4. feladat
+            Console.Write("Kérek egy karaktert: ");
+            char karakter = Console.ReadLine().ToUpper()[0];
+
+            try
+            {
+                string morzeKod = morzeKods.Find(kod => kod.Kulcs == karakter).Kod;
+
+                Console.WriteLine($"A {karakter} karakter morze kódja: {morzeKod}");
+            } catch
+            {
+                Console.WriteLine("Nem található a kódtárban ilyen karakter!");
+            }
+
+            // 5 .feladat
+            reader = new StreamReader("../../morze.txt");
+
+            while (!reader.EndOfStream)
+            {
+                string[] idezetAdatok = reader.ReadLine().Split(';'); // index: 0 - költő, 1 - idézet
+
+                morzeIdezetek.Add(new Idezet (idezetAdatok[0], idezetAdatok[1]));
+            }
+
+            reader.Close();
+
+            Console.WriteLine(Morze2Szöveg(morzeIdezetek[0].Kolto));
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 13.D/c#/20250313-robin/BerdoTamas_Focistak/BerdoTamas_Focistak/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BerdoTamas_Focistak
+{
+    internal class Focistak
+    {
+        public string Neve { get; set; }
+        public int SzuletesiEv { get; set; }
+        public string Poszt { get; set; }
+        public string Nemzetiseg { get; set; }
+        public Focistak(string neve, int szulev, string poszt, string nemzetiseg) {
+            this.Neve = neve;
+            this.SzuletesiEv = szulev;
+            this.Poszt = poszt;
+            this.Nemzetiseg = nemzetiseg;
+        }
+    }
+    internal class Program
+    {   
+        internal static List<Focistak> fociLista = new List<Focistak>();
+        static void Main(string[] args)
+        {
+            // 2. feladat
+            StreamReader reader = new StreamReader("../../focistak.csv");
+
+            reader.ReadLine();
+
+            while (!reader.EndOfStream)
+            {
+                string[] line = reader.ReadLine().Split(';');
+
+                fociLista.Add(new Focistak(line[0], Convert.ToInt16(line[1]), line[2], line[3]));
+            }
+
+            // 3. feladat
+            Console.WriteLine($"3.Feladat:\nAz adatszerkezet {fociLista.Count} példányt tartalmaz.");
+
+            // 4. feladat
+            Focistak legidosebbFocista = fociLista[0];
+            for (int i = 1; i < fociLista.Count; i++)
+            {
+                if(legidosebbFocista.SzuletesiEv > fociLista[i].SzuletesiEv)
+                {
+                    legidosebbFocista = fociLista[i];
+                }
+            }
+
+            Console.WriteLine($"4.Feladat\nA legidősebb játékos: {legidosebbFocista.Neve} ({legidosebbFocista.SzuletesiEv}), {legidosebbFocista.Poszt} - {legidosebbFocista.Nemzetiseg}");
+
+            // 5. feladat
+            Console.WriteLine("5.Feladat\nÍrjon be egy nemzetiséget:");
+            string keresettNemzetiseg = Console.ReadLine();
+            Focistak[] keresettNemzetiseguFocistak = fociLista.FindAll(focista => focista.Nemzetiseg == keresettNemzetiseg).ToArray();
+
+            if (keresettNemzetiseguFocistak.Length == 0)
+            {
+                Console.WriteLine("Nem található ilyen nemzetiség!");
+            }
+
+            foreach (Focistak focista in keresettNemzetiseguFocistak)
+            {
+                Console.WriteLine($"{focista.Neve} ({focista.SzuletesiEv}), {focista.Poszt} - {focista.Nemzetiseg}");
+            }
+
+            // 6. feladat
+            Console.WriteLine("6.Feladat\nÍrjon be egy posztot:");
+            string keresettPoszt = Console.ReadLine();
+            Focistak[] keresettPosztuFocistak = fociLista.FindAll(focista => focista.Poszt == keresettPoszt).ToArray();
+
+            if(keresettPosztuFocistak.Length == 0)
+            {
+                Console.WriteLine("Nem található ilyen poszt!");
+            }
+
+            foreach (Focistak focista in keresettPosztuFocistak)
+            {
+                Console.WriteLine($"{focista.Neve} ({focista.SzuletesiEv}), {focista.Poszt} - {focista.Nemzetiseg}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
