4344cc3c1117b7c247d43e0204983e8fcb9d04e0
Tomi <admin@mcitomi.hu>
2025-04-08 11:05:26 +0200

    db updated

--- 13.D/MM/2025.04.08/blog/backend/database/input.sql
+++ 13.D/MM/2025.04.08/blog/backend/database/input.sql
@@ -5,7 +5,6 @@ use blog;
 create table users(
     id int auto_increment primary key,
     username varchar(255) unique,
-    display_name varchar(255),
     password varchar(255)
 );
 
@@ -25,9 +24,9 @@ create table likes(
     foreign key (post_id) references posts(id)
 );
 
-insert into users (username, display_name, password) values
-("mmate4", "Molnár Máté", "$2a$12$bPXCO3TrtC3WD86DunFGheyReWviQ1pNj1gHCIET4mvEQU72n6/de"),
-("rob00", "Mizere Robin", "$2a$12$X1p4yr5KaUI25Ufm.XA2w..zUtvVKXdQMOy5h6UhijfQPXhXPQqlW");
+insert into users (username, password) values
+("mmate4", "$2a$12$bPXCO3TrtC3WD86DunFGheyReWviQ1pNj1gHCIET4mvEQU72n6/de"),
+("rob00", "$2a$12$X1p4yr5KaUI25Ufm.XA2w..zUtvVKXdQMOy5h6UhijfQPXhXPQqlW");
 
 insert into posts (user_id, title, content) values
 (1, "Próbavizsga időpontja", "Nem tudom."),
