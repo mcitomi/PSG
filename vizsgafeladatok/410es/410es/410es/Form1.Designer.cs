c99f46690209b5327e24ca02909cc3e533daa421
Tomi <admin@mcitomi.hu>
2025-05-18 20:51:51 +0200

    c# desktop app 410-es

--- vizsgafeladatok/410es/410es/410es/Form1.Designer.cs
+++ vizsgafeladatok/410es/410es/410es/Form1.Designer.cs
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboboxHonnan = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboboxHova = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboboxHonnan
+            // 
+            this.comboboxHonnan.FormattingEnabled = true;
+            this.comboboxHonnan.Location = new System.Drawing.Point(236, 111);
+            this.comboboxHonnan.Name = "comboboxHonnan";
+            this.comboboxHonnan.Size = new System.Drawing.Size(224, 21);
+            this.comboboxHonnan.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Honnan";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hová";
+            // 
+            // comboboxHova
+            // 
+            this.comboboxHova.FormattingEnabled = true;
+            this.comboboxHova.Location = new System.Drawing.Point(236, 165);
+            this.comboboxHova.Name = "comboboxHova";
+            this.comboboxHova.Size = new System.Drawing.Size(224, 21);
+            this.comboboxHova.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(257, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Szamol";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(247, 352);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Az utazás várható időtartama: x megálló";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboboxHova);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboboxHonnan);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboboxHonnan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboboxHova;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- vizsgafeladatok/410es/410es/410es/Form1.cs
+++ vizsgafeladatok/410es/410es/410es/Form1.cs
@@ -7,14 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace _410es
 {
     public partial class Form1 : Form
     {
+        public List<Allomasok> allomasokListaja = new List<Allomasok>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("../../V410.csv");
+
+            sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                string[] line = sr.ReadLine().Split(';');
+
+                allomasokListaja.Add(new Allomasok(line[0], Convert.ToInt32(line[1]), Convert.ToInt32(line[2])));
+
+                comboboxHonnan.Items.Add(line[0]);
+                comboboxHova.Items.Add(line[0]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Allomasok induloPont = allomasokListaja.Find(all => all.Megallo.Trim() == comboboxHonnan.SelectedItem.ToString());
+            Allomasok erkezesiPont = allomasokListaja.Find(all => all.Megallo.Trim() == comboboxHova.SelectedItem.ToString());
+
+            int megallokSzama = Math.Abs(induloPont.Hatvantol - erkezesiPont.Hatvantol);
+
+            label3.Text = $"Az utazás várható időtartama: {megallokSzama} megálló";
+        }
+    }
+
+    public class Allomasok
+    {
+        public string Megallo { get; set; }
+        public int Hatvantol { get; set; }
+        public int Stadiontol { get; set; }
+        public Allomasok(string megallo, int hatvantol, int stadiontol)
+        {
+            this.Megallo = megallo;
+            this.Hatvantol = hatvantol;
+            this.Stadiontol = stadiontol;
         }
     }
 }
